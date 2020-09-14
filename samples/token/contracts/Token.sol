ca91197ea51292be6a382bbac34de39725587da8
asa-Q <asa.qu@qq.com>
2020-09-14 15:50:55 +0800

    make a vault token

--- samples/token/contracts/Token.sol
+++ samples/token/contracts/Token.sol
@@ -1,126 +1,130 @@
-pragma solidity ^0.6.0;
-
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
 
 import "./ezMath.sol";
 import "./openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./openzeppelin/contracts/math/SafeMath.sol";
+import "./openzeppelin/contracts/utils/Address.sol";
+import "./openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./openzeppelin/contracts/GSN/Context.sol";
+
+import "../interfaces/Controller.sol";
 
 /**
     @title Bare-bones Token implementation
     @notice Based on the ERC-20 token standard as defined at
             https://eips.ethereum.org/EIPS/eip-20
  */
-contract Token {
+contract Token is ERC20 {
 
-    using ezMath for uint256;
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
 
-    string public symbol;
-    string public name;
-    uint256 public decimals;
-    uint256 public totalSupply;
+    IERC20 public token;
 
-    mapping(address => uint256) balances;
-    mapping(address => mapping(address => uint256)) allowed;
+    uint public min = 9500;
+    uint public constant max = 10000;
 
-    event Transfer(address from, address to, uint256 value);
-    event Approval(address owner, address spender, uint256 value);
+    address public governance;
+    address public controller;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _decimals,
-        uint256 _totalSupply
-    )
-        public
-    {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _totalSupply;
-        balances[msg.sender] = _totalSupply;
-        emit Transfer(address(0), msg.sender, _totalSupply);
+    constructor (address _token, address _controller) public ERC20(
+        string(abi.encodePacked("yearn ", ERC20(_token).name())),
+        string(abi.encodePacked("y", ERC20(_token).symbol()))
+    ) {
+        _setupDecimals(ERC20(_token).decimals());
+        token = IERC20(_token);
+        governance = msg.sender;
+        controller = _controller;
     }
 
-    /**
-        @notice Getter to check the current balance of an address
-        @param _owner Address to query the balance of
-        @return Token balance
-     */
-    function balanceOf(address _owner) public view returns (uint256) {
-        return balances[_owner];
+    function balance() public view returns (uint) {
+        return token.balanceOf(address(this))
+                .add(Controller(controller).balanceOf(address(token)));
     }
 
-    /**
-        @notice Getter to check the amount of tokens that an owner allowed to a spender
-        @param _owner The address which owns the funds
-        @param _spender The address which will spend the funds
-        @return The amount of tokens still available for the spender
-     */
-    function allowance(
-        address _owner,
-        address _spender
-    )
-        public
-        view
-        returns (uint256)
-    {
-        return allowed[_owner][_spender];
+    function setMin(uint _min) external {
+        require(msg.sender == governance, "!governance");
+        min = _min;
     }
 
-    /**
-        @notice Approve an address to spend the specified amount of tokens on behalf of msg.sender
-        @dev Beware that changing an allowance with this method brings the risk that someone may use both the old
-             and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
-             race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
-             https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-        @param _spender The address which will spend the funds.
-        @param _value The amount of tokens to be spent.
-        @return Success boolean
-     */
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowed[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
+    function setGovernance(address _governance) public {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
     }
 
-    /** shared logic for transfer and transferFrom */
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(balances[_from] >= _value, "Insufficient balance");
-        balances[_from] = balances[_from].sub(_value);
-        balances[_to] = balances[_to].add(_value);
-        emit Transfer(_from, _to, _value);
+    function setController(address _controller) public {
+        require(msg.sender == governance, "!governance");
+        controller = _controller;
     }
 
-    /**
-        @notice Transfer tokens to a specified address
-        @param _to The address to transfer to
-        @param _value The amount to be transferred
-        @return Success boolean
-     */
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        _transfer(msg.sender, _to, _value);
-        return true;
+    // Custom logic in here for how much the vault allows to be borrowed
+    // Sets minimum required on-hand to keep small withdrawals cheap
+    function available() public view returns (uint) {
+        return token.balanceOf(address(this)).mul(min).div(max);
     }
 
-    /**
-        @notice Transfer tokens from one address to another
-        @param _from The address which you want to send tokens from
-        @param _to The address which you want to transfer to
-        @param _value The amount of tokens to be transferred
-        @return Success boolean
-     */
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _value
-    )
-        public
-        returns (bool)
-    {
-        require(allowed[_from][msg.sender] >= _value, "Insufficient allowance");
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
-        _transfer(_from, _to, _value);
-        return true;
+    function earn() public {
+        uint _bal = available();
+        token.safeTransfer(controller, _bal);
+        Controller(controller).earn(address(token), _bal);
     }
+
+    function depositAll() external {
+        deposit(token.balanceOf(msg.sender));
+    }
+
+    function deposit(uint _amount) public {
+        uint _pool = balance();
+        uint _before = token.balanceOf(address(this));
+        token.safeTransferFrom(msg.sender, address(this), _amount);
+        uint _after = token.balanceOf(address(this));
+        _amount = _after.sub(_before); // Additional check for deflationary tokens
+        uint shares = 0;
+        if (totalSupply() == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount.mul(totalSupply())).div(_pool);
+        }
+        _mint(msg.sender, shares);
+    }
+
+    function withdrawAll() external {
+        withdraw(balanceOf(msg.sender));
+    }
+
+    // Used to swap any borrowed reserve over the debt limit to liquidate to 'token'
+    function harvest(address reserve, uint amount) external {
+        require(msg.sender == controller, "!controller");
+        require(reserve != address(token), "token");
+        IERC20(reserve).safeTransfer(controller, amount);
+    }
+
+    // No rebalance implementation for lower fees and faster swaps
+    function withdraw(uint _shares) public {
+        uint r = (balance().mul(_shares)).div(totalSupply());
+        _burn(msg.sender, _shares);
+
+        // Check balance
+        uint b = token.balanceOf(address(this));
+        if (b < r) {
+            uint _withdraw = r.sub(b);
+            Controller(controller).withdraw(address(token), _withdraw);
+            uint _after = token.balanceOf(address(this));
+            uint _diff = _after.sub(b);
+            if (_diff < _withdraw) {
+                r = b.add(_diff);
+            }
+        }
+
+        token.safeTransfer(msg.sender, r);
+    }
+
+    function getPricePerFullShare() public view returns (uint) {
+        return balance().mul(1e18).div(totalSupply());
+    }
+
 
 }
